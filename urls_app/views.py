2cfb5e37ad419bc91053ef7093cddeae35685f32
ultra-hash <jarviscommand3@gmail.com>
2023-01-17 13:54:48 +0530

    sample view in short and added url path for short view

--- urls_app/views.py
+++ urls_app/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World")
+
+
+def short(request, shorturl):
+    return HttpResponse("<h1>Hello "+ shorturl +"</h1>")
