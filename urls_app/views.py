fb7145478312d3ca55774be32ea48d23a62651ad
ultra-hash <jarviscommand3@gmail.com>
2023-01-17 14:21:28 +0530

    configure index.html template to index view

--- urls_app/views.py
+++ urls_app/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse(render(request, template_name='index.html'))
 
 
 def short(request, shorturl):
